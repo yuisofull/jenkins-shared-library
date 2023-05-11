9880745fccb6c2c655dde3e23f1c1d0eabce1bac
yuisofull <119509086+yuisofull@users.noreply.github.com>
2023-05-11 19:35:22 +0700

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,5 +3,7 @@
 import com.example.Docker
 
 def call(String imageName) {
+    //echo "building the docker image for $imageName"
+    //sh "docker build -t $imageName ."
     return new Docker(this).buildDockerImage(imageName)
 }
