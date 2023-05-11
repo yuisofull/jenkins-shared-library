bc8026cc83724c74ddedd1623dc646feac96e3ed
Your Name <you@example.com>
2023-05-11 09:52:00 +0000

    ok

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -12,9 +12,9 @@ class Docker implements Serializable {
 
 	def buildDockerImage(String imageName) {
 		script.echo "building the docker image for $imageName"
+		script.sh "docker build -t $imageName ."
 		script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         		srcipt.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
-        		script.sh "docker build -t $imageName ."
         		script.sh "docker push $imageName"
 		}
 	}
